fa2e54d4e8494ac9e8c30811fbfb1ebce62ef714
Adriano Junior Vidal <adriano.jv@hotmail.com>
2023-03-13 16:26:06 -0300

    HTML5 e Marcação 1/2

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ProjetoCurso-Danki-PHP-PrimeiroProjetoEmPhp
- Meu primeiro projeto em PHP do curso Desenvolvimento Web Completo da Danki Code
+
+Meu primeiro projeto em PHP do curso Desenvolvimento Web Completo da Danki Code
+
+<br/>
+
+### Curso Desenvolvimento Web Completo
+
+#### Projeto 01 - Crriando Site Dinâmico
+
+- HTML5 e Marcação
+-
+-
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <title>Projeto 01</title>
+    <meta charset="UTF-8">
+    
+    <!--META Responsividade -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    
+    <!--META TAGS PARA SEO -->
+    <meta name="keywords" content="palavras,chaves,do,meu,site">
+    <meta name="desciption" content="Descrição do meu site">
+
+</head>
+<body>
+
+    <header>
+        <div class="logo"> Logomarca </div>
+        <nav class="desktop">
+            <ul>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Serviços</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+        <nav class="mobile">
+            <ul>
+                <li>Home</li>
+                <li>Sobre</li>
+                <li>Serviços</li>
+                <li>Contato</li>
+            </ul>
+        </nav>
+    </header>
+
+    <section class="banner-principal">
+        <form>
+            <h2>Qual seu melhor e-mail</h2>
+            <input type="email" name="email" required>
+            <input type="submit" name="acao" value="Cadastrar">
+        </form>
+    </section>
+
+    <section class="descricao-autor">
+        <div class="w50">
+            <h2>Adriano Junior Vidal</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel tellus ante. Nunc tempor pellentesque tempor. Donec ultrices odio a venenatis sodales. Vestibulum fringilla ipsum dapibus nisl convallis tempor. Vestibulum vel lorem eu felis pellentesque volutpat. Aliquam luctus quam eu sodales porta. Donec quis ultricies augue. Vestibulum auctor molestie lorem sit amet porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec tempor leo eget dictum cursus.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel tellus ante. Nunc tempor pellentesque tempor. Donec ultrices odio a venenatis sodales. Vestibulum fringilla ipsum dapibus nisl convallis tempor. Vestibulum vel lorem eu felis pellentesque volutpat. Aliquam luctus quam eu sodales porta. Donec quis ultricies augue. Vestibulum auctor molestie lorem sit amet porta. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec tempor leo eget dictum cursus.</p>
+        </div>
+        <div class="w50">
+            <img src="/images/adrianojrv.jpg" alt="Minha foto">
+        </div>
+    </section>
+
+
+
+
+
+
+
+
+</body>
+</html>
