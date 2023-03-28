b5550d36673a9682b7bf556b386a4772608c33a9
Adriano Junior Vidal <adriano.jv@hotmail.com>
2023-03-28 20:23:39 -0300

    Mais de CSS3 3/3

--- index.php
+++ index.php
@@ -118,12 +118,12 @@
 				
 				<div class="depoimento-single">
 					<p class="depoimento-descricao">
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+						"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+						proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 					</p>
 					<p class="nome-autor">Lorem ipsum</p>
 				</div><!--depoimento-single-->
